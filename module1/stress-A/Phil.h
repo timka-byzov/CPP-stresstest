cf3b7044df5b77fabac714565578a8454d721f8b
timka-byzov <tb@lamart.com>
2023-09-26 12:10:16 +0500

    add namespaces in A

--- module1/stress-A/Phil.h
+++ module1/stress-A/Phil.h
@@ -2,98 +2,102 @@
 #include <vector>
 #include <algorithm>
 
-struct Subsequence {
-    std::vector<int> sequence;
-    int length = 0;
-    std::vector<int> indices;
-};
+namespace Phil{
+    struct Subsequence {
+        std::vector<int> sequence;
+        int length = 0;
+        std::vector<int> indices;
+    };
 
-Subsequence AlternatingSubsequence(std::vector<int> &arr, int totalNumber) {
+    Subsequence AlternatingSubsequence(std::vector<int> &arr, int totalNumber) {
 
-    std::vector<std::vector<Subsequence>> dp(totalNumber, std::vector<Subsequence>(2));
+        std::vector<std::vector<Subsequence>> dp(totalNumber, std::vector<Subsequence>(2));
 
-    for (int i = 0; i < totalNumber; ++i) {
-        dp[i][0].sequence.push_back(arr[i]);
-        dp[i][0].length = 1;
-        dp[i][0].indices.push_back(i);
-        dp[i][1].sequence.push_back(arr[i]);
-        dp[i][1].length = 1;
-        dp[i][1].indices.push_back(i);
-    }
+        for (int i = 0; i < totalNumber; ++i) {
+            dp[i][0].sequence.push_back(arr[i]);
+            dp[i][0].length = 1;
+            dp[i][0].indices.push_back(i);
+            dp[i][1].sequence.push_back(arr[i]);
+            dp[i][1].length = 1;
+            dp[i][1].indices.push_back(i);
+        }
 
-    std::vector<Subsequence> subsequences;
+        std::vector<Subsequence> subsequences;
 
-    for (int irod = 1; irod < totalNumber; ++irod) {
-        for (int jabroni = 0; jabroni < irod; ++jabroni) {
-            if (arr[jabroni] < arr[irod] && dp[irod][0].length < dp[jabroni][1].length + 1) {
-                dp[irod][0].sequence = dp[jabroni][1].sequence;
-                dp[irod][0].sequence.push_back(arr[irod]);
-                dp[irod][0].length = dp[jabroni][1].length + 1;
-                dp[irod][0].indices = dp[jabroni][1].indices;
-                dp[irod][0].indices.push_back(irod);
+        for (int irod = 1; irod < totalNumber; ++irod) {
+            for (int jabroni = 0; jabroni < irod; ++jabroni) {
+                if (arr[jabroni] < arr[irod] && dp[irod][0].length < dp[jabroni][1].length + 1) {
+                    dp[irod][0].sequence = dp[jabroni][1].sequence;
+                    dp[irod][0].sequence.push_back(arr[irod]);
+                    dp[irod][0].length = dp[jabroni][1].length + 1;
+                    dp[irod][0].indices = dp[jabroni][1].indices;
+                    dp[irod][0].indices.push_back(irod);
+                }
+
+                if (arr[jabroni] > arr[irod] && dp[irod][1].length < dp[jabroni][0].length + 1) {
+                    dp[irod][1].sequence = dp[jabroni][0].sequence;
+                    dp[irod][1].sequence.push_back(arr[irod]);
+                    dp[irod][1].length = dp[jabroni][0].length + 1;
+                    dp[irod][1].indices = dp[jabroni][0].indices;
+                    dp[irod][1].indices.push_back(irod);
+                }
             }
 
-            if (arr[jabroni] > arr[irod] && dp[irod][1].length < dp[jabroni][0].length + 1) {
-                dp[irod][1].sequence = dp[jabroni][0].sequence;
-                dp[irod][1].sequence.push_back(arr[irod]);
-                dp[irod][1].length = dp[jabroni][0].length + 1;
-                dp[irod][1].indices = dp[jabroni][0].indices;
-                dp[irod][1].indices.push_back(irod);
+            int maxLength = std::max(dp[irod][0].length, dp[irod][1].length);
+            if (maxLength > 1) {
+                if (dp[irod][0].length == maxLength) {
+                    subsequences.push_back(dp[irod][0]);
+                }
+                if (dp[irod][1].length == maxLength) {
+                    subsequences.push_back(dp[irod][1]);
+                }
             }
         }
 
-        int maxLength = std::max(dp[irod][0].length, dp[irod][1].length);
-        if (maxLength > 1) {
-            if (dp[irod][0].length == maxLength) {
-                subsequences.push_back(dp[irod][0]);
-            }
-            if (dp[irod][1].length == maxLength) {
-                subsequences.push_back(dp[irod][1]);
+        Subsequence smallestSubsequence;
+        smallestSubsequence.length = 0;
+        for (const Subsequence &subsequence: subsequences) {
+            if (subsequence.length > smallestSubsequence.length ||
+                (subsequence.length == smallestSubsequence.length &&
+                 subsequence.indices < smallestSubsequence.indices)) {
+                smallestSubsequence = subsequence;
             }
         }
+
+        return smallestSubsequence;
     }
 
-    Subsequence smallestSubsequence;
-    smallestSubsequence.length = 0;
-    for (const Subsequence &subsequence: subsequences) {
-        if (subsequence.length > smallestSubsequence.length ||
-            (subsequence.length == smallestSubsequence.length &&
-             subsequence.indices < smallestSubsequence.indices)) {
-            smallestSubsequence = subsequence;
+    std::vector<int> removeNeighboringDuplicates(const std::vector<int> &sequence) {
+        std::vector<int> result;
+        if (sequence.empty()) {
+            return result;
         }
-    }
 
-    return smallestSubsequence;
-}
+        result.push_back(sequence[0]);
 
-std::vector<int> removeNeighboringDuplicates(const std::vector<int> &sequence) {
-    std::vector<int> result;
-    if (sequence.empty()) {
+        for (size_t i = 1; i < sequence.size(); ++i) {
+            if (sequence[i] != sequence[i - 1]) {
+                result.push_back(sequence[i]);
+            }
+        }
+
         return result;
     }
 
-    result.push_back(sequence[0]);
 
-    for (size_t i = 1; i < sequence.size(); ++i) {
-        if (sequence[i] != sequence[i - 1]) {
-            result.push_back(sequence[i]);
+    std::vector<int> solve(std::vector<int> arr, int totalNumber) {
+
+        arr = removeNeighboringDuplicates(arr);
+        totalNumber = arr.size();
+
+        if (totalNumber == 1) {
+            return std::vector<int>{arr[0]};
         }
+        Subsequence result = AlternatingSubsequence(arr, totalNumber);
+        return result.sequence;
     }
 
-    return result;
+
+
 }
-
-
-std::vector<int> solve_Phil(std::vector<int> arr, int totalNumber) {
-
-    arr = removeNeighboringDuplicates(arr);
-    totalNumber = arr.size();
-
-    if (totalNumber == 1) {
-        return std::vector<int>{arr[0]};
-    }
-    Subsequence result = AlternatingSubsequence(arr, totalNumber);
-    return result.sequence;
-}
-
 
--- module1/stress-A/Tim.h
+++ module1/stress-A/Tim.h
@@ -4,132 +4,135 @@
 #include <algorithm>
 #include <vector>
 
-struct DP {
+namespace Tim{
+    struct DP {
 
-    std::vector<int> up, down;
-    int size;
+        std::vector<int> up, down;
+        int size;
 
-    explicit DP(int size) {
-        up.resize(size, 0);
-        down.resize(size, 0);
-        this->size = size;
-    }
-};
+        explicit DP(int size) {
+            up.resize(size, 0);
+            down.resize(size, 0);
+            this->size = size;
+        }
+    };
 
-struct Prev {
+    struct Prev {
 
-    std::vector<int> up, down;
+        std::vector<int> up, down;
 
-    explicit Prev(int size) {
-        up.resize(size, -1);
-        down.resize(size, -1);
-    }
+        explicit Prev(int size) {
+            up.resize(size, -1);
+            down.resize(size, -1);
+        }
 
-};
+    };
 
 // Есть два массива ДП, up[i] -- макс. длина чередующейся подпоследовательности из первых i элементов, такой,
 // что последний элемент был больше предыдущего (последовательность идёт вверх); down[i] -- наоборот вниз.
 // up[i] считается на основе down и наоборот
 // Соответственно для up есть массив down, а для down -- up, это нужно для восстановления ответа.
 // При чём предок выбирается так, чтобы удовлетворять условию о минимальности индексов
-void fill_dp_and_prev(DP &dp, Prev &prev, std::vector<int> &sequence) {
+    void fill_dp_and_prev(DP &dp, Prev &prev, std::vector<int> &sequence) {
 
-    for (int dp_idx = 0; dp_idx < dp.size; ++dp_idx) {
-        dp.up[dp_idx] = 1;
-        dp.down[dp_idx] = 1;
+        for (int dp_idx = 0; dp_idx < dp.size; ++dp_idx) {
+            dp.up[dp_idx] = 1;
+            dp.down[dp_idx] = 1;
 
-        int prev_up_idx = 0, prev_down_idx = 0;
+            int prev_up_idx = 0, prev_down_idx = 0;
 
-        for (int candidate_idx = 0; candidate_idx < dp_idx; ++candidate_idx) {
-            if (sequence[candidate_idx] > sequence[dp_idx]) {
-                if (1 + dp.up[candidate_idx] > dp.down[dp_idx] ||
-                    1 + dp.up[candidate_idx] == dp.down[dp_idx] && prev.down[candidate_idx] < prev_down_idx) {
-                    dp.down[dp_idx] = 1 + dp.up[candidate_idx];
-                    prev.up[dp_idx] = candidate_idx;
-                    prev_down_idx = prev.down[candidate_idx];
+            for (int candidate_idx = 0; candidate_idx < dp_idx; ++candidate_idx) {
+                if (sequence[candidate_idx] > sequence[dp_idx]) {
+                    if (1 + dp.up[candidate_idx] > dp.down[dp_idx] ||
+                        1 + dp.up[candidate_idx] == dp.down[dp_idx] && prev.down[candidate_idx] < prev_down_idx) {
+                        dp.down[dp_idx] = 1 + dp.up[candidate_idx];
+                        prev.up[dp_idx] = candidate_idx;
+                        prev_down_idx = prev.down[candidate_idx];
+                    }
                 }
-            }
 
-            if (sequence[candidate_idx] < sequence[dp_idx]) {
-                if (1 + dp.down[candidate_idx] > dp.up[dp_idx] ||
-                    1 + dp.down[candidate_idx] == dp.up[dp_idx] && prev.up[candidate_idx] < prev_up_idx) {
-                    dp.up[dp_idx] = 1 + dp.down[candidate_idx];
-                    prev.down[dp_idx] = candidate_idx;
-                    prev_up_idx = prev.up[candidate_idx];
+                if (sequence[candidate_idx] < sequence[dp_idx]) {
+                    if (1 + dp.down[candidate_idx] > dp.up[dp_idx] ||
+                        1 + dp.down[candidate_idx] == dp.up[dp_idx] && prev.up[candidate_idx] < prev_up_idx) {
+                        dp.up[dp_idx] = 1 + dp.down[candidate_idx];
+                        prev.down[dp_idx] = candidate_idx;
+                        prev_up_idx = prev.up[candidate_idx];
+                    }
                 }
             }
         }
     }
-}
 
 
-int get_max_subsequence_length(DP &dp) {
-    int max_length = 0;
-    for (int idx = 0; idx < dp.size; ++idx) {
-        max_length = std::max(std::max(max_length, dp.up[idx]), dp.down[idx]);
+    int get_max_subsequence_length(DP &dp) {
+        int max_length = 0;
+        for (int idx = 0; idx < dp.size; ++idx) {
+            max_length = std::max(std::max(max_length, dp.up[idx]), dp.down[idx]);
+        }
+        return max_length;
     }
-    return max_length;
-}
 
 // на основе 2х массивов предков восстанавливает ответ, если последним индексом из sequence в нём является idx
-std::vector<int> restore_ans_idxes(std::vector<int> &prev_one,
-                                   std::vector<int> &prev_two,
-                                   int idx) {
+    std::vector<int> restore_ans_idxes(std::vector<int> &prev_one,
+                                       std::vector<int> &prev_two,
+                                       int idx) {
 
-    std::vector<int> prev_one_copy(prev_one), prev_two_copy(prev_two);
+        std::vector<int> prev_one_copy(prev_one), prev_two_copy(prev_two);
 
-    std::vector<int> restored_ans;
-    while (prev_one_copy[idx] != -1) {
+        std::vector<int> restored_ans;
+        while (prev_one_copy[idx] != -1) {
+            restored_ans.push_back(idx);
+            idx = prev_one_copy[idx];
+            swap(prev_one_copy, prev_two_copy);
+        }
         restored_ans.push_back(idx);
-        idx = prev_one_copy[idx];
-        swap(prev_one_copy, prev_two_copy);
-    }
-    restored_ans.push_back(idx);
 
-    reverse(restored_ans.begin(), restored_ans.end());
-    return restored_ans;
-}
+        reverse(restored_ans.begin(), restored_ans.end());
+        return restored_ans;
+    }
 
 
 // Подпоследовательностей, максимальной длины может быть несколько.
 // Находим индекс, где оканчивается очередная и восстанавливаем все её индексы.
 // Далее выбираем ту, что удовлетворяет условию.
-std::vector<int> get_ans_indexes(DP &dp, Prev &prev) {
+    std::vector<int> get_ans_indexes(DP &dp, Prev &prev) {
 
-    int max_length = get_max_subsequence_length(dp);
+        int max_length = get_max_subsequence_length(dp);
 
-    std::vector<std::vector<int>> maybe_ans_idxes;
-    for (int idx = 0; idx < dp.size; ++idx) {
+        std::vector<std::vector<int>> maybe_ans_idxes;
+        for (int idx = 0; idx < dp.size; ++idx) {
 
-        if (dp.up[idx] == max_length) {
-            maybe_ans_idxes.push_back(restore_ans_idxes(prev.down, prev.up, idx));
+            if (dp.up[idx] == max_length) {
+                maybe_ans_idxes.push_back(restore_ans_idxes(prev.down, prev.up, idx));
+            }
+
+            if (dp.down[idx] == max_length) {
+                maybe_ans_idxes.push_back(restore_ans_idxes(prev.up, prev.down, idx));
+            }
         }
 
-        if (dp.down[idx] == max_length) {
-            maybe_ans_idxes.push_back(restore_ans_idxes(prev.up, prev.down, idx));
+        sort(maybe_ans_idxes.begin(), maybe_ans_idxes.end());
+        return maybe_ans_idxes[0];
+    }
+
+    std::vector<int> restore_ans(std::vector<int> idxes, std::vector<int> sequence) {
+        std::vector<int> ans;
+        for (auto idx: idxes) {
+            ans.push_back(sequence[idx]);
         }
+        return ans;
+
     }
 
-    sort(maybe_ans_idxes.begin(), maybe_ans_idxes.end());
-    return maybe_ans_idxes[0];
-}
 
-std::vector<int> restore_ans(std::vector<int> idxes, std::vector<int> sequence) {
-    std::vector<int> ans;
-    for (auto idx: idxes) {
-        ans.push_back(sequence[idx]);
+    std::vector<int> solve(std::vector<int> sequence, int totalNumber) {
+
+        DP dp(totalNumber);
+        Prev prev(totalNumber);
+        fill_dp_and_prev(dp, prev, sequence);
+
+
+        return restore_ans(get_ans_indexes(dp, prev), sequence);
     }
-    return ans;
 
-}
-
-
-std::vector<int> solve_Tim(std::vector<int> sequence, int totalNumber) {
-
-    DP dp(totalNumber);
-    Prev prev(totalNumber);
-    fill_dp_and_prev(dp, prev, sequence);
-
-
-    return restore_ans(get_ans_indexes(dp, prev), sequence);
 }
